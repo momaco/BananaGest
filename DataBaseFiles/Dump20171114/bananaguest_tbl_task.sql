cd4c1eab8d226c3e9a5a8c32395b481f2f19f4e0
Fernando Isaías García Aguirre <ferisagaragu@gmail.com>
2017-11-16 14:52:22 -0600

    Depuración de código
    Elimine archivos innecesarios

---
+++ DataBaseFiles/Dump20171114/bananaguest_tbl_task.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bananaguest
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_task`
+--
+
+DROP TABLE IF EXISTS `tbl_task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbl_task` (
+  `idTask` int(11) NOT NULL AUTO_INCREMENT,
+  `taskName` varchar(100) DEFAULT NULL,
+  `start` date DEFAULT NULL,
+  `finish` date DEFAULT NULL,
+  `idUser` int(11) NOT NULL,
+  `idProyect` int(11) NOT NULL,
+  PRIMARY KEY (`idTask`),
+  KEY `idUser` (`idUser`),
+  KEY `idProyect` (`idProyect`),
+  CONSTRAINT `tbl_task_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `tbl_user` (`idUser`),
+  CONSTRAINT `tbl_task_ibfk_2` FOREIGN KEY (`idProyect`) REFERENCES `tbl_proyect` (`idProyect`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_task`
+--
+
+LOCK TABLES `tbl_task` WRITE;
+/*!40000 ALTER TABLE `tbl_task` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_task` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-16 10:54:29
